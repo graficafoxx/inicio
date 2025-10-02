62f9a12a08bc5166e4f689bbc92aa677c4815b89
Fox <www.graficagrid.com@gmail.com>
2025-10-02 18:40:47 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/inicio/',        // caminho do repo no Pages
+  base: '/inicio/',
   plugins: [react()],
 })
