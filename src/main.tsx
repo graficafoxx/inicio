6a8da23b9aa6afc7327555f57c3bb69cff6f0323
Fox <www.graficagrid.com@gmail.com>
2025-10-02 19:02:54 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import './index.css'   // <- ESSENCIAL pro Vite gerar e injetar o CSS no build
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
